0c1348240e57c1cbc5cda0c82e0e5a526dd30124
cjgb550 <cjgb550@gmail.com>
2022-10-18 21:15:54 -0500

    pruebas pyton

--- hola-mundo.py
+++ hola-mundo.py
@@ -1,5 +1,30 @@
-print("Hola Mundo")
-print("hola")
-print("Hola Mundo")
-print("hola")
-print("hola")
+from ipaddress import summarize_address_range
+
+
+vehiculo = [{}]*2
+
+for compartimiento in range(2):
+    vehiculo[compartimiento] = {
+        "agua_en_botella": {"cantidad":1, "valor_unitario": 2},
+        "linterna_funcional": {"cantidad":1, "valor_unitario": 3}
+    }
+
+print(vehiculo)
+
+print ("")
+def suma (a=2, b=3):
+    return a+b
+
+s = suma 
+resultado = s(5,3)
+print(resultado)
+
+print ("")
+
+def duplicar(datos):
+    for i,n in enumerate(datos):
+       numeros[i] = numeros[i] * 2
+
+numeros = [10,5,2]
+duplicar(numeros)
+print(numeros)
